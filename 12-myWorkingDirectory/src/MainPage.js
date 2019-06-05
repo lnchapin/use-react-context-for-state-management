5b8f4676b3d67ec46218252af8cbf94decdce699
Lindsay Chapin <lnchapin@gmail.com>
2019-06-04 21:28:43 -0600

    adding login logout to context

--- 12-myWorkingDirectory/src/MainPage.js
+++ 12-myWorkingDirectory/src/MainPage.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Header from './Header';
 import MessageList from './MessageList';
 
-const MainPage = ({ currentUser, onLogout }) => (
+const MainPage = () => (
+
   <main>
-    <Header onLogout={onLogout} />
+    <Header />
     <MessageList />
   </main>
 );
